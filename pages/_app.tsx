b0f032ec9c9716ffa6e84d3439937247c4916643
Warman Suganda <warman.suganda@gmail.com>
2022-06-01 00:08:48 +0700

    style: set them provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+
+import theme from "../themes/default";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
