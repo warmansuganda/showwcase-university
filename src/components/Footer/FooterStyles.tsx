dd2ab3599689b1e2cf087bfa7e3bf35e66df31c9
Warman Suganda <warman.suganda@gmail.com>
2022-06-01 16:54:56 +0700

    feat(component): add footer component

---
+++ src/components/Footer/FooterStyles.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.footer`
+  padding: 12px;
+`;
+
+export const Content = styled.footer`
+  padding: 12px;
+  text-align: center;
+`;
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import Divider from "src/components/Divider";
+
+import { Wrapper, Content } from "./FooterStyles";
+
+function Footer() {
+  return (
+    <Wrapper>
+      <Divider />
+      <Content>Made with ❤ by Warman Suganda</Content>
+    </Wrapper>
+  );
+}
+
+export default Footer;
