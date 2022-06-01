3e4e3a753ea73c292e3081620ffdb754b903b4ee
Warman Suganda <warman.suganda@gmail.com>
2022-06-01 21:12:49 +0700

    feat(component): create container and flex component

---
+++ src/components/Flex/index.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import { variant } from "styled-system";
+
+import { SizeVariant, FlexAlign } from "src/utils/types";
+
+const gapVariant = variant({
+  prop: "gap",
+  variants: {
+    xs: {
+      gap: "4px",
+    },
+    sm: {
+      gap: "8px",
+    },
+    md: {
+      gap: "12px",
+    },
+    lg: {
+      gap: "16px",
+    },
+    xl: {
+      gap: "20px",
+    },
+  },
+});
+
+interface FlexProps {
+  gap?: SizeVariant;
+  direction?: "row" | "column";
+  justifyContent?: FlexAlign;
+  alignItems?: FlexAlign;
+}
+
+const Flex = styled.div<FlexProps>`
+  display: flex;
+  ${gapVariant};
+  direction: ${(props) => props.direction};
+  justify-content: ${(props) => props.justifyContent};
+  align-items: ${(props) => props.alignItems};
+`;
+
+Flex.defaultProps = {
+  gap: "md",
+  direction: "row",
+  justifyContent: "flex-start",
+  alignItems: "flex-start",
+};
+
+export default Flex;
