a1aac402193fc0016ccfeb8d61a27cee77871bb6
Warman Suganda <warman.suganda@gmail.com>
2022-06-01 16:29:41 +0700

    feat(utils): add common type

---
+++ src/utils/types.ts
@@ -0,0 +1,12 @@
+export type ColorVariant =
+  | "primary"
+  | "secondary"
+  | "success"
+  | "danger"
+  | "warning"
+  | "info"
+  | "light"
+  | "dark"
+  | "neutral";
+
+export type SizeVariant = "xs" | "sm" | "md" | "lg" | "xl";
