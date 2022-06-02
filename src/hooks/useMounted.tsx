aebafa3db34127ec5fbeff7d70067198e8e66f06
Warman Suganda <warman.suganda@gmail.com>
2022-06-02 22:58:56 +0700

    feat(hook): add useMounted hook

---
+++ src/hooks/useMounted.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+
+const useMounted = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+    return () => setMounted(false);
+  }, []);
+
+  return { mounted };
+};
+
+export default useMounted;
