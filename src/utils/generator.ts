e92b8b47dfa287652583814078555ec2d20c3449
Warman Suganda <warman.suganda@gmail.com>
2022-06-03 02:50:03 +0700

    feat(utils): add uuid generator and slugify function

--- src/utils/generator.ts
+++ src/utils/generator.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-bitwise */
 import moment from "moment";
 
 export function generateYears(
@@ -17,4 +18,12 @@ export function generateYears(
 export function generateMonths() {
   const months = moment.months();
   return months.map((label, index) => ({ value: index + 1, label }));
+}
+
+export function uuid() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
