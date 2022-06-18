ff2b056471d68485a0d9266bbf5776a329866c96
Warman Suganda <warman.suganda@gmail.com>
2022-06-19 01:46:34 +0700

    feat: pwa

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+
+const nextConfig = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    // skipWaiting: true,
+    // swSrc: "service-worker.js",
+    runtimeCaching,
+  },
   reactStrictMode: true,
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true,
   },
-};
+});
 
 module.exports = nextConfig;
